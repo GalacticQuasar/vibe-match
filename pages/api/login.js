e64b88e39197c6007833760852b1b60f03f54b01
GalacticQuasar <akashdfw@gmail.com>
2024-10-26 17:46:39 -0400

    Get track attributes

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,4 @@
 export default function handler(req, res) {
-    const scope = 'user-read-private user-read-email user-top-read';
-    res.redirect(`https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=${scope}`);
-  }
-  
+  const scope = 'user-read-private user-read-email user-top-read';
+  res.redirect(`https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=${scope}`);
+}
--- pages/playlists.js
+++ pages/playlists.js
@@ -1,24 +1,35 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Playlists = () => {
-  const [playlists, setPlaylists] = useState([]);
+const Tracks = () => {
+  const [tracks, setTracks] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchPlaylists = async () => {
+    const fetchTracks = async () => {
       // Check if we're in the browser
       if (typeof window !== 'undefined') {
         const accessToken = new URLSearchParams(window.location.search).get('access_token');
         if (accessToken) {
           try {
-            const response = await axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10', {
+            const response = await axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=20', {
               headers: {
                 Authorization: `Bearer ${accessToken}`,
               },
             });
-            setPlaylists(response.data.items);
+
+            for (let i = 0; i < response.data.items.length; i++) {
+              const anudder = await axios.get(`https://api.spotify.com/v1/audio-features/${response.data.items[i].id}`, {
+                headers: {
+                  Authorization: `Bearer ${accessToken}`,
+                },
+              });
+
+              console.log(anudder)
+            }
+
+            setTracks(response.data.items);
           } catch (err) {
             setError(err.message);
           } finally {
@@ -31,7 +42,7 @@ const Playlists = () => {
       }
     };
 
-    fetchPlaylists();
+    fetchTracks();
   }, []);
 
   if (loading) return <div>Loading...</div>;
@@ -39,14 +50,14 @@ const Playlists = () => {
 
   return (
     <div>
-      <h1>Your Playlists</h1>
+      <h1>Your Top Tracks</h1>
       <ul>
-        {playlists.map((playlist) => (
-          <li key={playlist.id}>{playlist.name}</li>
+        {tracks.map((track) => (
+          <li key={track.id}>{track.id}</li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default Playlists;
+export default Tracks;
