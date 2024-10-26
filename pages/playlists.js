f61cb6a3bf69d2daf2b9165d728d402aa632a1af
GalacticQuasar <akashdfw@gmail.com>
2024-10-26 18:28:24 -0400

    Average features into user object

--- pages/playlists.js
+++ pages/playlists.js
@@ -13,21 +13,55 @@ const Tracks = () => {
         const accessToken = new URLSearchParams(window.location.search).get('access_token');
         if (accessToken) {
           try {
+            // Get all tracks
             const response = await axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=20', {
               headers: {
                 Authorization: `Bearer ${accessToken}`,
               },
             });
 
-            for (let i = 0; i < response.data.items.length; i++) {
-              const audioFeatures = await axios.get(`https://api.spotify.com/v1/audio-features/${response.data.items[i].id}`, {
+            let userFeatures = {
+              id: "",
+              instrumentalness: 0,
+              acousticness: 0,
+              energy: 0,
+              speechiness: 0,
+              valence: 0,
+              danceability: 0,
+              liveness: 0,
+            };
+            
+            const promises = response.data.items.map(async (item) => {
+              const audioFeatures = await axios.get(`https://api.spotify.com/v1/audio-features/${item.id}`, {
                 headers: {
                   Authorization: `Bearer ${accessToken}`,
                 },
               });
-
-              console.log(audioFeatures);
+              
+              // Make sure to access the data correctly
+              const features = audioFeatures.data;
+              
+              userFeatures.instrumentalness += features.instrumentalness;
+              userFeatures.acousticness += features.acousticness;
+              userFeatures.energy += features.energy;
+              userFeatures.speechiness += features.speechiness;
+              userFeatures.valence += features.valence;
+              userFeatures.danceability += features.danceability;
+              userFeatures.liveness += features.liveness;
+            });
+            
+            // Wait for all promises to resolve
+            await Promise.all(promises);
+            
+            // Calculate averages
+            const trackCount = response.data.items.length;
+            if (trackCount > 0) {
+              Object.keys(userFeatures).forEach(key => {
+                userFeatures[key] /= trackCount;
+              });
             }
+            
+            console.log(userFeatures);            
 
             setTracks(response.data.items);
           } catch (err) {
